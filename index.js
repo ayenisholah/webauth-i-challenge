7f91c5d68ae08ffd74992607edd0285de844ec1d
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-22 12:25:36 +0100

    endpoint to register new user

--- index.js
+++ index.js
@@ -13,7 +13,22 @@ server.use(cors());
 
 server.get('/', (req, res) => {
   res.send('Hello World')
-})
+});
+
+server.post('/api/register', (req, res) => {
+  let user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12);
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+
 
 const port = process.env.Port || 7777;
 server.listen(port, () => console.log(`\n >>>> Magic happening on 7777 <<<< \n`));
