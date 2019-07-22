be89bb7b11e3e1a3e1e1032878971caa2c004980
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-22 12:30:57 +0100

    Endpoint to login

--- index.js
+++ index.js
@@ -28,7 +28,22 @@ server.post('/api/register', (req, res) => {
     });
 });
 
+server.post('/api/login', (req, res) => {
+  let { username, password } = req.body;
 
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 const port = process.env.Port || 7777;
 server.listen(port, () => console.log(`\n >>>> Magic happening on 7777 <<<< \n`));
