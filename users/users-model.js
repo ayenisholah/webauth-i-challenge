bafe0303310d5a1470f4f1f5a8747dc99e05d11b
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-22 12:05:20 +0100

    find helper function

---
+++ users/users-model.js
@@ -0,0 +1,16 @@
+const db = require('../database/dbConfig.js');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+};
+
+function find() {
+  return db('users').select('id', 'username', 'password');
+}
+
+
+
+
